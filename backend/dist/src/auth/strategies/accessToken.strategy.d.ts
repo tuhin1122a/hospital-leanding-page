2e17f277d260e90c03e393fb9c0f15b2f4ba0110
unknown <tuhinrahmna48@gmail.com>
2026-04-05 09:03:04 +0600

    chore: separate backend into its own repository

--- backend/dist/src/auth/strategies/accessToken.strategy.d.ts
+++
@@ -1,9 +0,0 @@
-import { Strategy } from 'passport-jwt';
-declare const AccessTokenStrategy_base: new (...args: [opt: import("passport-jwt").StrategyOptionsWithRequest] | [opt: import("passport-jwt").StrategyOptionsWithoutRequest]) => Strategy & {
-    validate(...args: any[]): unknown;
-};
-export declare class AccessTokenStrategy extends AccessTokenStrategy_base {
-    constructor();
-    validate(payload: any): any;
-}
-export {};
